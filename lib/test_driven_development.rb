7c1bc705907a26dee3ca131082b0314f6c83efcd
Tony Won <tonywon89@gmail.com>
2016-03-16 13:59:34 -0700

    finish array exercises w/ TDD, implemented/tested stock_picker

--- lib/test_driven_development.rb
+++ lib/test_driven_development.rb
@@ -21,6 +21,35 @@ class Array
 
     pairs
   end
+
+  def my_transpose
+    return [] if empty?
+    raise ArgumentError if any? { |el| !el.is_a?(Array) }
+    transposed = Array.new(first.length) { Array.new }
+    i = 0
+    while i < transposed.length
+      self.each do |row|
+        transposed[i] << row[i]
+      end
+      i += 1
+    end
+    transposed
+  end
 end
 
-p [1, -1].two_sums
+def stock_picker(prices)
+  return nil if prices.size < 2
+  best_pair = nil
+  highest_profit = nil
+  prices.each_with_index do |current_price, c_day|
+    current_profit =  nil
+    prices.drop(c_day+1).each_with_index do |future_price, f_day|
+      current_profit = future_price - current_price
+      if highest_profit.nil? || current_profit > highest_profit
+        highest_profit = current_profit
+        best_pair = [c_day, (f_day + c_day + 1)]
+      end
+    end
+  end
+  best_pair
+end
--- spec/test_driven_development_spec.rb
+++ spec/test_driven_development_spec.rb
@@ -41,4 +41,81 @@ describe "Array" do
       expect([-1, 0, 2, -2, 1].two_sums).to eq([[0, 4], [2, 3]])
     end
   end
+
+  describe "#my_transpose" do
+    it "handles empty array" do
+      expect([].my_transpose).to eq([])
+    end
+
+    it "raise error if array contains non-array items" do
+      expect {[1].my_transpose}.to raise_error(ArgumentError)
+    end
+
+    context "multidimensional arrays" do
+      let(:square_matrix) do
+        [
+         [1,2,3],
+         [4,5,6],
+         [7,8,9]
+        ]
+      end
+
+      let(:transposed_square) do
+        [
+          [1,4,7],
+          [2,5,8],
+          [3,6,9]
+        ]
+      end
+
+      let(:rect_matrix) do
+        [
+          [1,2,3],
+          [4,5,6]
+        ]
+      end
+
+      let(:transposed_rec) do
+        [
+          [1,4],
+          [2,5],
+          [3,6]
+        ]
+      end
+
+      it "transposes a square matrix" do
+        expect(square_matrix.my_transpose).to eq(transposed_square)
+      end
+
+      it "transposes a rectangular matrix" do
+        expect(rect_matrix.my_transpose).to eq(transposed_rec)
+      end
+
+      it "returns original matrix if transposed twice" do
+        expect(rect_matrix.my_transpose.my_transpose).to eq(rect_matrix)
+      end
+    end
+  end
+end
+
+describe "#stock_picker" do
+  it "handles an empty array" do
+    expect(stock_picker([])).to eq(nil)
+  end
+
+  it "return nil if only one price" do
+    expect(stock_picker([42])).to eq(nil)
+  end
+
+  it "finds the most profitable pair of days" do
+    expect(stock_picker([12, 4, 20])).to eq([1, 2])
+  end
+
+  it "doesn't go back in time to sell" do
+    expect(stock_picker([12, 4, 20, 3])).to eq([1, 2])
+  end
+
+  it "minimizes losses" do
+    expect(stock_picker([12, 10, 7, 3])).to eq([0, 1])
+  end
 end
